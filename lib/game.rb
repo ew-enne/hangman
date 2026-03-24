8222951236ebcf41640e6436adaf2a28d3b521e9
ewenne <221577967+ew-enne@users.noreply.github.com>
2026-03-24 13:28:23 +0100

    Added saving and loading to/from file
    This completes the first version of the program.

--- lib/game.rb
+++ lib/game.rb
@@ -17,55 +17,96 @@ class Game
   end
 
 
-
   def play
 
-    countdown = 8
-
-    word = open_file.chomp
-    until (word.length > 4  && word.length < 13)
-      # puts word.length
-      word = open_file.chomp
-    end
-    puts "The secret word is: #{word}"
-
-    # initialize the array showing the guesses of the player
-    array_result = Array.new(word.length) { '-' }   
-
-    # initialize the array containing the wrong guesses
+    # initialize variables
+    countdown = 0
+    array = []
+    array_result = []
     array_wrong = []
-        
-    # transform the random word into an array
-    array = word.chars
+
+    ####### option to load a saved game ######
+    puts
+    print "Would you like to load a saved game (y/n)? "
+    player_choice = gets.chomp
+
+    if player_choice == "y" ### start with a saved game
+      puts
+      File.open("game.txt", "r") do |game_file|
+        countdown = game_file.readline().to_i # the saved remaining turns
+        array = game_file.readline().chomp.chars # the array with selected random word
+        array_result = game_file.readline().chomp.chars # the array with the saved correct characters
+        array_wrong = game_file.readline().chomp.chars # the array with the saved wrong characters
+      end
+      puts "Here are your saved guesses: #{array_result.join}"
+      puts "Here are your wrong guesses: #{array_wrong.join}"
+      puts "You have #{countdown} turns left"
+
+    else ### start with a new game
+      puts
+      puts "Ok, so let's start a new game."
+      countdown = 8
+
+      word = open_file.chomp
+      until (word.length > 4  && word.length < 13)
+        word = open_file.chomp
+      end
+
+      # transform the random word into an array
+      array = word.chars
+
+      # initialize the array showing the guesses of the player
+      array_result = Array.new(word.length) { '-' }
+
+      # initialize the array containing the wrong guesses
+      array_wrong = []
+    end
 
     while (array_result.include?('-') && countdown > 0)
 
-      # launches a guessing turn
-      turn = Turn.new
+      ####### option to save a game ######
       puts
-      player_entry = turn.enter_character.downcase
+      print "Would you like to save the game (y/n)? "
+      save_choice = gets.chomp
 
-      # checks if the entered character is present in the array
-      character_present = array.include?(player_entry)
-      
-      if !character_present
-        array_wrong << player_entry
+      if save_choice == "y" ### save the game
         puts
-        puts array_result.join
+        File.open("game.txt", "w") do |game_file|
+          game_file.puts countdown.to_s
+          game_file.puts array.join
+          game_file.puts array_result.join
+          game_file.puts array_wrong.join
+        end
+        exit
+
+      else ### continue the game
+        # launches a guessing turn
+        turn = Turn.new
         puts
-        puts "This character is NOT present in the word. Try again!"
-      else
-        puts
-        puts "This character is present in the word."
-        array.each_with_index do |letter, index|
-          if letter == player_entry
-            array_result[index] = player_entry
+        player_entry = turn.enter_character.downcase
+
+        # checks if the entered character is present in the array
+        character_present = array.include?(player_entry)
+        
+        if !character_present
+          array_wrong << player_entry
+          puts
+          puts array_result.join
+          puts
+          puts "This character is NOT present in the word. Try again!"
+        else
+          puts
+          puts "This character is present in the word."
+          array.each_with_index do |letter, index|
+            if letter == player_entry
+              array_result[index] = player_entry
+            end
           end
+          puts
+          puts array_result.join
         end
         puts
-        puts array_result.join
       end
-      puts
 
       countdown -= 1
       if countdown > 0
@@ -75,13 +116,12 @@ class Game
         end
       else
         puts "Game over, you don't have any guesses left!"
+        puts "The secret word was: #{array.join}"
         exit
       end
     end
     puts "You won!"
 
   end
-
-
 
 end
